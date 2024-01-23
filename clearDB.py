d4432d218b840c13c7a38ad9391f69b0082bd2d6
Rusy13 <rusugarov@gmail.com>
2024-01-23 23:18:14 +0300

    extra directories have been deleted

--- clearDB.py
+++ clearDB.py
@@ -1,9 +1,7 @@
 import os
 from sqlalchemy import create_engine, MetaData
-# from src.database import database_url
 from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 database_url = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-# from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 
 engine = create_engine(database_url)
--- src/config.py
+++ src/config.py
@@ -8,19 +8,4 @@ DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
-
-# DBT_HOST = os.environ.get("DBT_HOST")
-# DBT_PORT = os.environ.get("DBT_PORT")
-# DBT_NAME = os.environ.get("DBT_NAME")
-# DBT_USER = os.environ.get("DBT_USER")
-# DBT_PASS = os.environ.get("DBT_PASS")
-
-
-# REDIS_HOST = os.environ.get("REDIS_HOST")
-# REDIS_PORT = os.environ.get("REDIS_PORT")
-
-# SECRET_AUTH = os.environ.get("SECRET_AUTH")
 SECRET_AUTH = "my_secret_key"
-
-# SMTP_USER = os.environ.get("SMTP_USER")
-# SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD")
